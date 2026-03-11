8c612b101291da9d9982e94ca0eb43e59d15e9bf
Paduan <gabrielpaduancofani@gmail.com>
2026-03-11 13:07:23 -0300

    Início da implementação do ambiente de frontend

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,9 @@
+import {Box, Typography} from '@mui/material'
+
+export function Login() {
+    return (
+        <Box display={"flex"} justifyContent={"center"} alignItems={"center"} height={"100vh"}>
+            <Typography variant="h1">Login</Typography>
+        </Box>
+    )
+}
